07b5296a2c4d2b47bbb842dfbe97bdfacd518b58
sany45 <sanybhuiyan97@gmail.com>
2024-04-04 21:28:43 +0600

    added-moment

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee";
 import { Link } from "react-router-dom";
 const BreakingNews = () => {
     return (
-        <div>
+        <div className="flex">
             <button className="btn btn-secondary">Breaking News</button>
             <Marquee pauseOnHover={true} speed={150}>
               <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
